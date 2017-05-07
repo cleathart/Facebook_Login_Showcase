8d2d1b689fd2a0af159c0450593daba4c87a74f4
Cuthbert Leathart <cl15341@it033848.users.bris.ac.uk>
2017-05-07 12:02:26 +0100

    Initial Commit

---
+++ Facebook_Login_Showcase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Facebook_Login_Showcase
+//
+//  Created by Cuthbert Leathart on 07/05/2017.
+//  Copyright © 2017 Cuthbert Leathart. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Facebook_Login_ShowcaseTests/Facebook_Login_ShowcaseTests.swift
@@ -0,0 +1,36 @@
+//
+//  Facebook_Login_ShowcaseTests.swift
+//  Facebook_Login_ShowcaseTests
+//
+//  Created by Cuthbert Leathart on 07/05/2017.
+//  Copyright © 2017 Cuthbert Leathart. All rights reserved.
+//
+
+import XCTest
+@testable import Facebook_Login_Showcase
+
+class Facebook_Login_ShowcaseTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
